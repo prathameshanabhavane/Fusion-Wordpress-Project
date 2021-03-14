94026e582c66bb98df70da399fd0f90555e6b732
prathameshanabhavane <pratham291994@gmail.com>
2021-03-14 15:20:24 +0530

    added contact and services layout

--- wp-content/themes/understrap-child/footer.php
+++ wp-content/themes/understrap-child/footer.php
@@ -15,27 +15,62 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
-<div class="wrapper" id="wrapper-footer">
+<div class="wrapper footer" id="wrapper-footer">
 
 	<div class="<?php echo esc_attr( $container ); ?>">
 
 		<div class="row">
-
-			<div class="col-md-12">
-
-				<footer class="site-footer" id="colophon">
-
-					<div class="site-info">
-
-						<?php understrap_site_info(); ?>
-
-					</div><!-- .site-info -->
-
-				</footer><!-- #colophon -->
-
-			</div><!--col end -->
-
-		</div><!-- row end -->
+			<div class="col-12 col-md-8 mx-auto">
+				<div class="row">
+					<div class="col-12 col-md-3">
+						<?php
+						$image = get_field('footer_logo');
+						if( !empty( $image ) ): ?>
+						    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+						<?php endif; ?>
+					</div>
+					<?php if( have_rows('addresses') ): ?>
+					    <?php while( have_rows('addresses') ): the_row();
+					        ?>
+							<div class="col-12 col-md-3">
+						       <h4>
+						       		<?php the_sub_field('title'); ?>
+						       </h4>
+							   <p>
+							   		<?php the_sub_field('content'); ?>
+							   </p>
+							</div>
+					    <?php endwhile; ?>
+					<?php endif; ?>
+					<?php if( have_rows('other_contact') ): ?>
+					    <?php while( have_rows('other_contact') ): the_row();
+					        ?>
+							<div class="col-12 col-md-3">
+						       <h4>
+						       		<?php the_sub_field('title'); ?>
+						       </h4>
+							   <p>
+							   		<?php the_sub_field('email'); ?>
+							   </p>
+							   <ul>
+								   <?php if( have_rows('social_links') ): ?>
+						   			    <?php while( have_rows('social_links') ): the_row();
+											$icon = get_sub_field('icon');
+						   			        ?>
+											<li>
+									   			<a href="<?php the_sub_field('link'); ?>">
+													<?php echo wp_get_attachment_image( $image, 'full' ); ?>
+									   			</a>
+									   		</li>
+						   			    <?php endwhile; ?>
+						   			<?php endif; ?>
+							   </ul>
+							</div>
+					    <?php endwhile; ?>
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
 
 	</div><!-- container end -->
 
@@ -48,4 +83,3 @@ $container = get_theme_mod( 'understrap_container_type' );
 </body>
 
 </html>
-
--- wp-content/themes/understrap-child/home.php
+++ wp-content/themes/understrap-child/home.php
@@ -1,5 +1,110 @@
 <?php /* Template Name: Home */  ?>
 <?php get_header(); ?>
 
+    <div class="home-page">
+        <section class="section-services">
+            <div class="container">
+                <div class="row header-element">
+                    <div class="col-12 col-md-8 mx-auto">
+                        <h6 class="text-uppercase">
+                            Our Services
+                        </h6>
+                        <h2>
+                            <?php echo get_field('header_content'); ?>
+                        </h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-10 mx-auto">
+                        <div class="row">
+                            <?php
+                                $categories = get_categories( array(
+                                    'orderby' => 'name',
+                                    'order'   => 'ASC'
+                                ) );
+
+                                foreach( $categories as $category ) : ?>
+                                    <div class="col-12 col-md-4 service-grid">
+                                     <a href="<?php echo get_category_link($category->term_id); ?>">
+                                        <?php
+                                        $image = get_field('icon', $category);
+                                        if( !empty( $image ) ): ?>
+                                            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                                        <?php endif; ?>
+                                        <h3>
+                                            <?php echo $category->name; ?>
+                                        </h3>
+                                        <p>
+                                            <?php echo $category->description; ?>
+                                        </p>
+                                        <span class="see-details-btn">
+                                            See detalis
+                                        </span>
+                                     </a>
+                                    </div>
+                            <?php endforeach; ?>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-contact">
+            <div class="container">
+                <div class="row header-element">
+                    <div class="col-12">
+                        <h6 class="text-uppercase">
+                            Conact us
+                        </h6>
+                        <h2>
+                            <?php echo get_field('contact_header_content'); ?>
+                        </h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-8 mx-auto">
+                        <div class="contact-element">
+                            <div class="row">
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label for="name">Your Name</label>
+                                        <input type="text" class="form-control" id="name">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Your Email</label>
+                                        <input type="email" class="form-control" id="exampleInputEmail1">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label for="company">Company</label>
+                                        <input type="text" class="form-control" id="company">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label for="subject">Subject</label>
+                                        <input type="email" class="form-control" id="subject">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 col-md-12">
+                                    <div class="form-group pb-0">
+                                        <label for="message">Your Message</label>
+                                        <input type="text" class="form-control" id="message">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 
 <?php get_footer(); ?>
